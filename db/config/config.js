1d55f37efea516686f418a9368a4cf44f4eb637a
jopespfs <juaumpedro2@gmail.com>
2023-11-09 21:52:26 -0300

    segundo commit

---
+++ db/config/config.js
@@ -0,0 +1,9 @@
+export const DB_CONFIG = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    pass: '1234',
+    db: 'dripstore',
+    dialect: 'mysql'
+    
+}
---
+++ model/produto.model.js
@@ -0,0 +1,39 @@
+import { DataTypes } from "sequelize";
+
+const produtoModel = (sequelize, Sequelize) => {
+  const produto = sequelize.define(
+    "produto",
+    {
+      // Model attributes are defined here
+      nome: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      categoria: {
+        type: DataTypes.STRING,
+        // allowNull defaults to true
+      },
+      descricao: {
+        type: DataTypes.STRING,
+        // allowNull defaults to true
+      },
+      desconto: {
+        type: DataTypes.INTEGER,
+        // allowNull defaults to true
+      },
+      precoAntes: {
+        type: DataTypes.DECIMAL,
+        // allowNull defaults to true
+      },
+      precoDepois: {
+        type: DataTypes.DECIMAL,
+        // allowNull defaults to true
+      },
+      ativo: {
+        type: DataTypes.BOOLEAN,
+        // allowNull defaults to true
+      },
+    });
+
+    return produto
+};
