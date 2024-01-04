77de3fff10da65337fc9f1986d3f99477a7f1de3
JOÃO PEDRO SILVEIRA PEREIRA DE FIGUEIREDO SANTOS <JOO0520@DIGITALCOLLEGE.LOCAL>
2024-01-04 18:43:22 -0300

    cors

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import db from "./model/index.js";
 import { routeProduto } from "./routes/produto.routes.js";
+import cors from "cors";
 
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
 
 //app.use(express.urlencoded())
 
